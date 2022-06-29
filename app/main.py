b3a4453810827a937baf0ef8541eab980732adda
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-06-28 21:42:35 -0300

    handle mocked create/list portfolios

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+portfolios: list = []
+
+
+class Portfolio(BaseModel):
+    name: str
+
+
+@app.get("/portfolios")
+def list_portfolios():
+    return {"portfolios": portfolios}
+
+
+@app.post("/portfolios")
+def create_portfolio(portfolio: Portfolio):
+    portfolios.append(portfolio)
+    return {"portfolios": portfolios}
 
 
 @app.get("/")
