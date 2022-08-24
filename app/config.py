5df704d3751c010933a39ac6ad392b479fc217ce
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-08-24 13:31:35 -0300

    conditionally check postgres individual env var

--- app/config.py
+++ app/config.py
@@ -3,14 +3,13 @@ from starlette.datastructures import Secret
 
 config = Config(".env")
 
-POSTGRES_USER = config("POSTGRES_USER", cast=str)
-POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
-POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
-POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default="5432")
-POSTGRES_DB = config("POSTGRES_DB", cast=str)
+DATABASE_URL = config("DATABASE_URL ", cast=str, default=None)
 
-DATABASE_URL = config(
-    "DATABASE_URL ",
-    cast=str,
-    default=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}",
-)
+if DATABASE_URL is None:
+    POSTGRES_USER = config("POSTGRES_USER", cast=str)
+    POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
+    POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
+    POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default="5432")
+    POSTGRES_DB = config("POSTGRES_DB", cast=str)
+
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
