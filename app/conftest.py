fc19c7b612267237e1edaee34f137d38037ede31
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-05 23:46:38 -0300

    handle deleting test database

--- app/conftest.py
+++ app/conftest.py
@@ -1,38 +1,18 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
 import os
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 os.environ["DATABASE_URL"] = SQLALCHEMY_DATABASE_URL
 
 
 from app.main import app
-from app.api.dependencies import get_db
-from app.db.init_db import init_db
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-init_db(engine)
-
-
-def override_get_db():
-    """Dependency for getting a database session"""
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @pytest.fixture()
 def client():
-    app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
+    if os.path.exists("test.db"):
+        os.remove("test.db")
