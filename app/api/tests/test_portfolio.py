4ee681b4c8010b4a7769e03cbcafa70f75c41257
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-06 11:46:17 -0300

    create test for create_portfolio

--- app/api/tests/test_portfolio.py
+++ app/api/tests/test_portfolio.py
@@ -1,4 +1,10 @@
-def test_list_portfolios(client):
-    response = client.get("/portfolios")
-    assert response.status_code == 200
-    assert response.json() == {"portfolios": []}
+class TestPortfolioApi:
+    def test_create_portfolio(self, client):
+        response = client.post("/portfolios/", json={"name": "Stocks"})
+        assert response.status_code == 200
+        assert response.json() == {"portfolio": {"id": 1, "name": "Stocks"}}
+
+    def test_list_portfolios(self, client):
+        response = client.get("/portfolios")
+        assert response.status_code == 200
+        assert response.json() == {"portfolios": []}
