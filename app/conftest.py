c94948e58c749b87bf894894f637ed36e6377876
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-06 11:43:43 -0300

    handle creating db between tests

--- app/conftest.py
+++ app/conftest.py
@@ -9,10 +9,13 @@ os.environ["DATABASE_URL"] = SQLALCHEMY_DATABASE_URL
 
 
 from app.main import app
+from app.db.database import engine
+from app.db.init_db import init_db
 
 
 @pytest.fixture()
 def client():
+    init_db(engine)
     yield TestClient(app)
     if os.path.exists("test.db"):
         os.remove("test.db")
