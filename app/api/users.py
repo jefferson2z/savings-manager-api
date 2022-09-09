7d004422d441383c5b306aea7f18f68876d9cc4b
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-09 12:20:15 -0300

    fix delete_user function name

--- app/api/users.py
+++ app/api/users.py
@@ -38,7 +38,7 @@ def get_user(
 
 
 @router.delete("/{user_id}")
-def delete_portfolio(user_id: int, db: Session = Depends(dependencies.get_db)):
+def delete_user(user_id: int, db: Session = Depends(dependencies.get_db)):
     db_user = users_crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
