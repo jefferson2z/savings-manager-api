cf4a15ad90d38722ccb1f9b621e02effab42e25e
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-14 13:12:41 -0300

    import models directly from package

--- app/crud/users_crud.py
+++ app/crud/users_crud.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import Session
 
 from app.security import get_password_hash, verify_password
-from app.models.user import User
+from app import models
 from app.schemas import user_schema
 
 
 def create_user(db: Session, user_create: user_schema.UserCreate):
     user_json = user_create.dict()
-    db_user = User(
+    db_user = models.User(
         username=user_json.get("username"),
         password_hash=get_password_hash(user_json.get("password")),
     )
@@ -18,15 +18,15 @@ def create_user(db: Session, user_create: user_schema.UserCreate):
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
+    return db.query(models.User).filter(models.User.id == user_id).first()
 
 
 def get_user_by_username(db: Session, username: str):
-    return db.query(User).filter(User.username == username).first()
+    return db.query(models.User).filter(models.User.username == username).first()
 
 
 def delete_user(db: Session, user_id: int):
-    db_user = db.query(User).get(user_id)
+    db_user = db.query(models.User).get(user_id)
     db.delete(db_user)
     db.commit()
     return db_user
