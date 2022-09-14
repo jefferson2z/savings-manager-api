237c9c1b1ef4a74edcd295868700e99db8125898
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-14 10:24:23 -0300

    handle user deleting own account

--- app/api/tests/test_users.py
+++ app/api/tests/test_users.py
@@ -13,12 +13,11 @@ class TestUserApi:
         )
         assert response.json() == {"id": "1", "username": "Ikari"}
 
-    def test_delete_user(self, client):
-        post_response = client.post(
-            "/users/", json={"username": "Ikari", "password": "safe password"}
+    def test_delete_user(self, client, jwt):
+        response = client.delete(
+            f"/users/me",
+            headers={"Authorization": f"Bearer {jwt}"},
         )
-        created_user = post_response.json()
-        response = client.delete(f"/users/{created_user.get('id')}")
         assert response.status_code == 200
 
     def test_update_user(self, client):
--- app/api/users.py
+++ app/api/users.py
@@ -26,10 +26,13 @@ def create_user(
     return db_user
 
 
-@router.delete("/{user_id}")
-def delete_user(user_id: int, db: Session = Depends(dependencies.get_db)):
-    db_user = users_crud.get_user(db, user_id=user_id)
+@router.delete("/me")
+def delete_user(
+    db: Session = Depends(dependencies.get_db),
+    current_user: user_schema.User = Depends(get_current_user),
+):
+    db_user = users_crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    db_user = users_crud.delete_user(db, user_id=user_id)
+    db_user = users_crud.delete_user(db, user_id=current_user.id)
     return {"user": db_user}
