1c97a714c0e20a557342a2ac1b7fdefa76f998d5
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-15 20:16:14 -0300

    return portfolios on users

--- app/api/tests/test_users.py
+++ app/api/tests/test_users.py
@@ -4,14 +4,14 @@ class TestUserApi:
             "/users/", json={"username": "Ikari", "password": "safe password"}
         )
         assert response.status_code == 201
-        assert response.json() == {"id": "1", "username": "Ikari"}
+        assert response.json() == {"id": "1", "username": "Ikari", "portfolios": []}
 
     def test_get_me_user(self, client, jwt):
         response = client.get(
             "/users/me",
             headers={"Authorization": f"Bearer {jwt}"},
         )
-        assert response.json() == {"id": "1", "username": "Ikari"}
+        assert response.json() == {"id": "1", "username": "Ikari", "portfolios": []}
 
     def test_delete_user(self, client, jwt):
         response = client.delete(
@@ -19,7 +19,7 @@ class TestUserApi:
             headers={"Authorization": f"Bearer {jwt}"},
         )
         assert response.status_code == 200
-        assert response.json() == {"id": "1", "username": "Ikari"}
+        assert response.json() == {"id": "1", "username": "Ikari", "portfolios": []}
 
     def test_update_user(self, client):
         pass
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from app.schemas import portfolio_schema
+
 
 class UserBase(BaseModel):
     username: str
@@ -15,6 +17,7 @@ class User(UserBase):
 
 class UserOutput(UserBase):
     id: str
+    portfolios: list[portfolio_schema.Portfolio]
 
     class Config:
         orm_mode = True
