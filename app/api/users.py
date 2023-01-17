b9c2740b98121ff9da6687436627b49f4d6c8b82
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2023-01-17 16:39:53 -0300

    fix delete_user types

--- app/api/users.py
+++ app/api/users.py
@@ -31,7 +31,7 @@ def delete_user(
     db: Session = Depends(dependencies.get_db),
     current_user: user_schema.User = Depends(get_current_user),
 ):
-    db_user = users_crud.get_user(db, user_id=current_user.id)
+    db_user = users_crud.get_user(db, user_id=str(current_user.id))
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     db_user = users_crud.delete_user(db, user_id=current_user.id)
--- app/crud/users_crud.py
+++ app/crud/users_crud.py
@@ -23,7 +23,7 @@ def create_user(db: Session, user_create: user_schema.UserCreate):
     return db_user
 
 
-def get_user(db: Session, user_id: int):
+def get_user(db: Session, user_id: str):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
@@ -31,7 +31,7 @@ def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
 
-def delete_user(db: Session, user_id: int):
+def delete_user(db: Session, user_id: str):
     db_user = db.query(models.User).get(user_id)
     db.delete(db_user)
     db.commit()
